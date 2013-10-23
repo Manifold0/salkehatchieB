c915c63c51a1309c3428b5a0923410ba54b14656
Nathan Cox <nathan.cox@furman.edu>
2013-10-23 17:03:53 -0400

    Created rails app to hold project

---
+++ SalkehatchieB/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SalkehatchieB::Application.config.secret_key_base = 'f377565bf06cfd5e760ea8156c797d1c115b354527a46ffe956032143584f7b8a402addfbe6df74d829e957575a313d55842dd4ecedc65b29411d207470996b9'
---
+++ SalkehatchieB/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SalkehatchieB::Application.config.session_store :cookie_store, key: '_SalkehatchieB_session'
