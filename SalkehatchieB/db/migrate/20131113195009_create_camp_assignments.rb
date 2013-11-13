a6f2a77880fb85efc9a7803025f4c46849069ea4
Nathan Cox <nathan.cox@furman.edu>
2013-11-13 15:34:33 -0500

    -fix create_camp_assignments migration

--- SalkehatchieB/db/migrate/20131113195009_create_camp_assignments.rb
+++ SalkehatchieB/db/migrate/20131113195009_create_camp_assignments.rb
@@ -3,7 +3,7 @@ class CreateCampAssignments < ActiveRecord::Migration
     create_table :camp_assignments do |t|
       t.references :user, index: true
       t.references :camp, index: true
-      t.int :permission_level
+      t.integer :permission_level
 
       t.timestamps
     end
