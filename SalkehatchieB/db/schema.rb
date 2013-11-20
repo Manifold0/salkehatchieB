aa9b7e2814ada68fe3745d62b5e8b8d077b8a407
Alison Hodges <hodgescam@gmail.com>
2013-11-20 14:39:36 -0500

    sign in page and sign up page

--- SalkehatchieB/db/schema.rb
+++ SalkehatchieB/db/schema.rb
@@ -56,7 +56,6 @@ ActiveRecord::Schema.define(version: 20131120182344) do
     t.date     "end_date"
     t.string   "city"
     t.string   "hq_address"
-    t.integer  "max_campers"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
