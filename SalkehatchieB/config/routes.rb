e456fd8608c557d3731ad77e953b50525754edea
Nathan Cox <querky1231@aim.com>
2013-12-16 23:34:40 -0500

    -make sites a subpage of camps

--- SalkehatchieB/config/routes.rb
+++ SalkehatchieB/config/routes.rb
@@ -7,7 +7,10 @@ SalkehatchieB::Application.routes.draw do
   get 'camps/:campid/campers' => "users#campers" , as: :camps_campers
   get 'camps/:campid/forms' => "camps#forms" , as: :camps_forms
   get 'camps/:campid/home' => "camps#home"
-  #get 'camps/:campid/sites' => "sites", as: :camps_sites
+
+  scope 'camps/:campid' do
+    resources :sites, as: :camps_sites
+  end
 
   get 'camps/:campid/queries' => "queries#index_for_directors", as: :directors_queries
   scope 'camps/:campid/queries' do
